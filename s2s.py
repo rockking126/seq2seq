5a5102c3be7894e4c9bc1788c5d4bf58bc1b48ac
rock <rockking126@gmail.com>
2019-07-26 23:08:17 +0800

    完成适配代码

--- s2s.py
+++ s2s.py
@@ -11,18 +11,21 @@ from keras.models import Model
 from keras import backend as K
 from keras.callbacks import Callback
 from keras.optimizers import Adam
-
+from keras.utils import multi_gpu_model
 import pandas as pd
 
 min_count = 32
-max_len = 144000
+max_len = 400
 batch_size = 64
 epochs = 100
-char_size = 128
+char_size = 388  # 128
 z_dim = 128
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'  # 使用 GPU 0，1
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
 # os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2" #这里指定GPU8
 # db = pymongo.MongoClient().text.thucnews # 我的数据存在mongodb中
 data = pd.read_csv("11w_mian.csv")
 word_list = pd.read_csv("word_list.csv")
@@ -35,37 +38,35 @@ if os.path.exists('seq2seq_config.json'):
 else:
     chars = {}
     for a in data:
-        for w in data['content_list']: # 纯文本，不用分词
+        for w in data['content_list']:  # 纯文本，不用分词
             chars[w] = chars.get(w, 0) + 1
-        for w in data['tittle']: # 纯文本，不用分词
+        for w in data['tittle']:  # 纯文本，不用分词
             chars[w] = chars.get(w, 0) + 1
     chars = {i: j for i, j in chars.items() if j >= min_count}
     # 0: mask
     # 1: unk
     # 2: start
     # 3: end
-    id2char = {i+4:j for i,j in enumerate(chars)}
-    char2id = {j:i for i,j in id2char.items()}
-    json.dump([chars,id2char,char2id], open('seq2seq_config.json', 'w'))
+    id2char = {i + 4: j for i, j in enumerate(chars)}
+    char2id = {j: i for i, j in id2char.items()}
+    json.dump([chars, id2char, char2id], open('seq2seq_config.json', 'w'))
 
 
-def str2id(s, start_end=False):
+def str2id(s, start_end=True):
     # 文字转整数id
     idd = []
-    if start_end: # 补上<start>和<end>标记
-        ids = [char2id.get(c, 1) for c in s[:max_len-2]]
+    s = str(s) + ' '
+    if start_end:  # 补上<start>和<end>标记
+        ids = [char2id.get(str(c), 1) for c in str(s[:max_len - 2])]
         ids = [2] + ids + [3]
-    else: # 普通转化
+    else:  # 普通转化
         for c in s:
             for d in c[:max_len]:
                 idd.append(char2id.get(d, 1))
         # ids = [char2id.get(c, 1) for c in s[:max_len]]
-    for c in s[:max_len]:
-        # print(max_len)
-        pass
-    # print(c + ':')
     # return ids
-    return idd
+    return ids
+
 
 def id2str(ids):
     # id转文字，找不到的用空字符代替
@@ -74,35 +75,50 @@ def id2str(ids):
 
 def padding(x):
     # padding至batch内的最大长度
-    print(x)
+    # print(x)
     ml = max([len(i) for i in x])
-    return [i + [0] * (ml-len(i)) for i in x]
+    return [i + [0] * (ml - len(i)) for i in x]
+
+
+# def data_generator():
+#     # 数据生成器
+#     X,Y = [],[]
+#     while True:
+#         for index, a in data.iterrows():
+#             X.append(a['content_list'])
+#             Y.append(a['tittle'])
+#             # print(a['tittle'])
+#             # print(len(Y))
+#             if len(X) == batch_size:
+#                 # bb = padding(X)
+#                 # cc = padding(Y)
+#                 X = np.array(str2id(X, start_end=True))
+#                 Y = np.array(str2id(Y, start_end=True))
+#                 yield [X,Y], None
+#                 # print(Y)
+#                 X,Y = [],[]
 
 
 def data_generator():
     # 数据生成器
-    X,Y = [],[]
+    X, Y = [], []
     while True:
         for index, a in data.iterrows():
-            X.append(a['content_list'])
-            Y.append(a['tittle'])
-            # print(a['tittle'])
-            # print(len(Y))
+            X.append(str2id(a['content_list']))
+            Y.append(str2id(a['tittle']))
             if len(X) == batch_size:
-                # bb = padding(X)
-                # cc = padding(Y)
-                X = np.array(str2id(X, start_end=True))
-                Y = np.array(str2id(Y, start_end=True))
-                yield [X,Y], None
-                # print(Y)
-                X,Y = [],[]
+                X = np.array(padding(X))
+                Y = np.array(padding(Y))
+                yield [X, Y], None
+                X, Y = [], []
+
 
 def to_one_hot(x):
     """输出一个词表大小的向量，来标记该词是否在文章出现过
     """
     x, x_mask = x
     x = K.cast(x, 'int32')
-    x = K.one_hot(x, len(chars)+4)
+    x = K.one_hot(x, len(chars) + 4)
     x = K.sum(x_mask * x, 1, keepdims=True)
     x = K.cast(K.greater(x, 0.5), 'float32')
     return x
@@ -111,16 +127,19 @@ def to_one_hot(x):
 class ScaleShift(Layer):
     """缩放平移变换层（Scale and shift）
     """
+
     def __init__(self, **kwargs):
         super(ScaleShift, self).__init__(**kwargs)
+
     def build(self, input_shape):
-        kernel_shape = (1,) * (len(input_shape)-1) + (input_shape[-1],)
+        kernel_shape = (1,) * (len(input_shape) - 1) + (input_shape[-1],)
         self.log_scale = self.add_weight(name='log_scale',
                                          shape=kernel_shape,
                                          initializer='zeros')
         self.shift = self.add_weight(name='shift',
                                      shape=kernel_shape,
                                      initializer='zeros')
+
     def call(self, inputs):
         x_outs = K.exp(self.log_scale) * inputs + self.shift
         return x_outs
@@ -129,6 +148,7 @@ class ScaleShift(Layer):
 class OurLayer(Layer):
     """定义新的Layer，增加reuse方法，允许在定义Layer时调用现成的层
     """
+
     def reuse(self, layer, *args, **kwargs):
         if not layer.built:
             if len(args) > 0:
@@ -153,18 +173,21 @@ class OurLayer(Layer):
 class OurBidirectional(OurLayer):
     """自己封装双向RNN，允许传入mask，保证对齐
     """
+
     def __init__(self, layer, **args):
         super(OurBidirectional, self).__init__(**args)
         self.forward_layer = copy.deepcopy(layer)
         self.backward_layer = copy.deepcopy(layer)
         self.forward_layer.name = 'forward_' + self.forward_layer.name
         self.backward_layer.name = 'backward_' + self.backward_layer.name
+
     def reverse_sequence(self, x, mask):
         """这里的mask.shape是[batch_size, seq_len, 1]
         """
         seq_len = K.round(K.sum(mask, 1)[:, 0])
         seq_len = K.cast(seq_len, 'int32')
         return K.tf.reverse_sequence(x, seq_len, seq_dim=1)
+
     def call(self, inputs):
         x, mask = inputs
         x_forward = self.reuse(self.forward_layer, x)
@@ -173,6 +196,7 @@ class OurBidirectional(OurLayer):
         x_backward = self.reverse_sequence(x_backward, mask)
         x = K.concatenate([x_forward, x_backward], 2)
         return x * mask
+
     def compute_output_shape(self, input_shape):
         return (None, input_shape[0][1], self.forward_layer.units * 2)
 
@@ -200,9 +224,11 @@ class SelfModulatedLayerNormalization(OurLayer):
     """模仿Self-Modulated Batch Normalization，
     只不过降Batch Normalization改为Layer Normalization
     """
+
     def __init__(self, num_hidden, **kwargs):
         super(SelfModulatedLayerNormalization, self).__init__(**kwargs)
         self.num_hidden = num_hidden
+
     def build(self, input_shape):
         super(SelfModulatedLayerNormalization, self).build(input_shape)
         output_dim = input_shape[0][-1]
@@ -211,6 +237,7 @@ class SelfModulatedLayerNormalization(OurLayer):
         self.beta_dense_2 = Dense(output_dim)
         self.gamma_dense_1 = Dense(self.num_hidden, activation='relu')
         self.gamma_dense_2 = Dense(output_dim)
+
     def call(self, inputs):
         inputs, cond = inputs
         inputs = self.reuse(self.layernorm, inputs)
@@ -222,6 +249,7 @@ class SelfModulatedLayerNormalization(OurLayer):
             beta = K.expand_dims(beta, 1)
             gamma = K.expand_dims(gamma, 1)
         return inputs * (gamma + 1) + beta
+
     def compute_output_shape(self, input_shape):
         return input_shape[0]
 
@@ -229,6 +257,7 @@ class SelfModulatedLayerNormalization(OurLayer):
 class Attention(OurLayer):
     """多头注意力机制
     """
+
     def __init__(self, heads, size_per_head, key_size=None,
                  mask_right=False, **kwargs):
         super(Attention, self).__init__(**kwargs)
@@ -237,11 +266,13 @@ class Attention(OurLayer):
         self.out_dim = heads * size_per_head
         self.key_size = key_size if key_size else size_per_head
         self.mask_right = mask_right
+
     def build(self, input_shape):
         super(Attention, self).build(input_shape)
         self.q_dense = Dense(self.key_size * self.heads, use_bias=False)
         self.k_dense = Dense(self.key_size * self.heads, use_bias=False)
         self.v_dense = Dense(self.out_dim, use_bias=False)
+
     def mask(self, x, mask, mode='mul'):
         if mask is None:
             return x
@@ -252,6 +283,7 @@ class Attention(OurLayer):
                 return x * mask
             else:
                 return x - (1 - mask) * 1e10
+
     def call(self, inputs):
         q, k, v = inputs[:3]
         v_mask, q_mask = None, None
@@ -272,7 +304,7 @@ class Attention(OurLayer):
         kw = K.permute_dimensions(kw, (0, 2, 1, 3))
         vw = K.permute_dimensions(vw, (0, 2, 1, 3))
         # Attention
-        a = K.batch_dot(qw, kw, [3, 3]) / self.key_size**0.5
+        a = K.batch_dot(qw, kw, [3, 3]) / self.key_size ** 0.5
         a = K.permute_dimensions(a, (0, 3, 2, 1))
         a = self.mask(a, v_mask, 'add')
         a = K.permute_dimensions(a, (0, 3, 2, 1))
@@ -287,6 +319,7 @@ class Attention(OurLayer):
         o = K.reshape(o, (-1, K.shape(o)[1], self.out_dim))
         o = self.mask(o, q_mask, 'mul')
         return o
+
     def compute_output_shape(self, input_shape):
         return (input_shape[0][0], input_shape[0][1], self.out_dim)
 
@@ -301,9 +334,9 @@ x_mask = Lambda(lambda x: K.cast(K.greater(K.expand_dims(x, 2), 0), 'float32'))(
 y_mask = Lambda(lambda x: K.cast(K.greater(K.expand_dims(x, 2), 0), 'float32'))(y)
 
 x_one_hot = Lambda(to_one_hot)([x, x_mask])
-x_prior = ScaleShift()(x_one_hot) # 学习输出的先验分布（标题的字词很可能在文章出现过）
+x_prior = ScaleShift()(x_one_hot)  # 学习输出的先验分布（标题的字词很可能在文章出现过）
 
-embedding = Embedding(len(chars)+4, char_size)
+embedding = Embedding(len(chars) + 4, char_size)
 x = embedding(x)
 y = embedding(y)
 
@@ -328,8 +361,8 @@ xy = Concatenate()([y, xy])
 # 输出分类
 xy = Dense(char_size)(xy)
 xy = LeakyReLU(0.2)(xy)
-xy = Dense(len(chars)+4)(xy)
-xy = Lambda(lambda x: (x[0]+x[1])/2)([xy, x_prior]) # 与先验结果平均
+xy = Dense(len(chars) + 4)(xy)
+xy = Lambda(lambda x: (x[0] + x[1]) / 2)([xy, x_prior])  # 与先验结果平均
 xy = Activation('softmax')(xy)
 
 # 交叉熵作为loss，但mask掉padding部分
@@ -338,6 +371,7 @@ cross_entropy = K.sum(cross_entropy * y_mask[:, 1:, 0]) / K.sum(y_mask[:, 1:, 0]
 
 model = Model([x_in, y_in], xy)
 model.add_loss(cross_entropy)
+model = multi_gpu_model(model, gpus=2)
 model.compile(optimizer=Adam(1e-3))
 
 
@@ -345,25 +379,25 @@ def gen_sent(s, topk=3, maxlen=64):
     """beam search解码
     每次只保留topk个最优候选结果；如果topk=1，那么就是贪心搜索
     """
-    xid = np.array([str2id(s)] * topk) # 输入转id
-    yid = np.array([[2]] * topk) # 解码均以<start>开头，这里<start>的id为2
-    scores = [0] * topk # 候选答案分数
-    for i in range(maxlen): # 强制要求输出不超过maxlen字
-        proba = model.predict([xid, yid])[:, i, 3:] # 直接忽略<padding>、<unk>、<start>
-        log_proba = np.log(proba + 1e-6) # 取对数，方便计算
-        arg_topk = log_proba.argsort(axis=1)[:,-topk:] # 每一项选出topk
-        _yid = [] # 暂存的候选目标序列
-        _scores = [] # 暂存的候选目标序列得分
+    xid = np.array([str2id(s)] * topk)  # 输入转id
+    yid = np.array([[2]] * topk)  # 解码均以<start>开头，这里<start>的id为2
+    scores = [0] * topk  # 候选答案分数
+    for i in range(maxlen):  # 强制要求输出不超过maxlen字
+        proba = model.predict([xid, yid])[:, i, 3:]  # 直接忽略<padding>、<unk>、<start>
+        log_proba = np.log(proba + 1e-6)  # 取对数，方便计算
+        arg_topk = log_proba.argsort(axis=1)[:, -topk:]  # 每一项选出topk
+        _yid = []  # 暂存的候选目标序列
+        _scores = []  # 暂存的候选目标序列得分
         if i == 0:
             for j in range(topk):
-                _yid.append(list(yid[j]) + [arg_topk[0][j]+3])
+                _yid.append(list(yid[j]) + [arg_topk[0][j] + 3])
                 _scores.append(scores[j] + log_proba[0][arg_topk[0][j]])
         else:
             for j in range(topk):
-                for k in range(topk): # 遍历topk*topk的组合
-                    _yid.append(list(yid[j]) + [arg_topk[j][k]+3])
+                for k in range(topk):  # 遍历topk*topk的组合
+                    _yid.append(list(yid[j]) + [arg_topk[j][k] + 3])
                     _scores.append(scores[j] + log_proba[j][arg_topk[j][k]])
-            _arg_topk = np.argsort(_scores)[-topk:] # 从中选出新的topk
+            _arg_topk = np.argsort(_scores)[-topk:]  # 从中选出新的topk
             _yid = [_yid[k] for k in _arg_topk]
             _scores = [_scores[k] for k in _arg_topk]
         yid = np.array(_yid)
@@ -376,12 +410,14 @@ def gen_sent(s, topk=3, maxlen=64):
     return id2str(yid[np.argmax(scores)])
 
 
-s1 = u'夏天来临，皮肤在强烈紫外线的照射下，晒伤不可避免，因此，晒后及时修复显得尤为重要，否则可能会造成长期伤害。专家表示，选择晒后护肤品要慎重，芦荟凝胶是最安全，有效的一种选择，晒伤严重者，还请及时就医 。'
+s1 = u'သႀကၤန္ကာလအတြင္း ယာဥ္မေတာ္တဆမႈ၊ မူခင္းႀကိဳတင္ကာကြယ္ေရးနဲ႔ လုံၿခဳံေရး အတြက္ တိုင္းရင္းသား လက္နက္ကိုင္အဖဲြ႔အစည္းေတြနဲ႔ပူးေပါင္းၿပီး ေရွာင္တခင္စစ္ေဆး ေဆာင္ရြက္သြားမယ္လို႔ ဘားအံၿမိဳ႕၊ အမွတ္ ၇၀ ယာဥ္ထိန္းတပ္ဖဲြ႔စုု၊ ေခတၱတပ္ဖဲြ႔မႉး ရဲအုပ္ ၀င္းသန္းက ေျပာပါတယ္။သႀကၤန္မွာ ယာဥ္အႏၱရာယ္ေလ်ာ့နည္းေစဖုိ႔၊ လမ္းစည္းကမ္းလိုက္နာ ဖို႔၊ပါ၀ါအိတ္ေဇာ တပ္ေမာင္းႏွင္တာေတြ၊ ေပါက္ကဲြေစတတ္တဲ့ လက္နက္ခဲယမ္းနဲ႔ မူးယစ္ဆး၀ါး သယ္ေဆာင္ လာတာေတြမရွိေစဖို႔အတြက္ ရည္ရြယ္ၿပီး ေဆာင္ရြက္တာလို႔ ဆိုပါတယ္။，ရဲအုပ္၀င္းသန္းက တိုင္းရင္းသား လက္နက္ကိုင္အဖဲြ႔အစည္းေတြကေတာ့ သူတို႔ရဲ႕ မိသားစုတပ္ဖဲြ႔၀င္ေတြ စီးႏွင္းေနတဲဲ့ယာဥ္ေတြက်ေတာ့ က်ေနာ္တို႔က အလုံးစုံမသိႏိုင္ဘူး။ မသိႏိုင္တဲ့အခါက်ေတာ့ က်ေနာ္တို႔ကို၀ိုင္း၀န္းကူညီၿပီး ေဆာင္ရြက္ေပးတာျဖစ္ပါတယ္လို႔ ေျပာပါတယ္။ေကအန္ယူ၊ ေကအန္အန္လ္ေအၿငိမ္းခ်မ္းေရးေကာင္စီ ဆက္ဆံေရး႐ုံးတာ၀န္ခံ ဗုိလ္မႉး ေစာေအာင္ခ်စ္က ၿငိမ္းခ်မ္းေရးအဖဲြ႔က ကိုယ္ဟာကိုယ္ထိန္းမယ္ဆိုရင္ေတာ့ အဆင္ေျပမယ္လို႔ ထင္တယ္။ က်ေနာ္တို႔ဆီကေတာ့ တတ္ႏိုင္သေလာက္ ႀကိဳးစားၿပီးေတာ့ ၀ိုင္း၀န္းထိန္းေပးတာ။ မူးယစ္ေဆး၀ါးတို႔ က်န္တဲ့ကိစၥလည္း အမ်ားႀကီး ပါတဲ့အတြက္ေၾကာင့္လို႔ ေျပာပါတယ္။ဧၿပီ ၁ ရက္ကစၿပီး ယာဥ္မေတာ္တဆမႈ၊ မူခင္းႀကိဳတင္ကာကြယ္ေရးနဲ႔ လုံၿခဳံေရးအတြက္ တိုင္းရင္းသားလက္နက္ကိုင္အဖဲြ႔အစည္းေတြ၊ ဌာနဆိုင္ရာေတြ၊ တပ္မေတာ္၊ လူမႈေရးအသင္းအဖဲြ႔ေတြနဲ႔ ပူးေပါင္းၿပီး ဆိုင္ကယ္နဲ႔ ေမာ္ေတာ္ယာဥ္ေတြကို စစ္ေဆးေနပါတယ္။မြန္ျပည္နယ္၊ မုဒုံၿမိဳ႕၊ ဦးေမာင္ေမာင္ကသႀကၤန္အခါရက္အတြင္းမွာ ေသာက္စား မူးယစ္တာရွိရင္ေတာ့ အႏၱရာယ္ရွိမွာပဲ။ မေသာက္တာအေကာင္းဆုံးပဲ။ ယာဥ္ေမာင္းတဲ့ သူတိုင္းက ေသာက္ၿပီးလို႔ရွိရင္ အသိဉာဏ္ ကင္းလြတ္သြားတဲ့အတြက္ အႏၱရာယ္ျဖစ္တတ္ တယ္လို႔ ေျပာပါတယ္။၂၀၁၈ မဟာသႀကၤန္ကာလမွာ ကရင္ျပည္နယ္အတြင္း ယာဥ္မေတာ္တဆမႈ ၁၀ မႈျဖစ္ပြားခဲ့ၿပီး၊ ဒဏ္ရာ ရရွိသူ က်ား ၁၆ ဦး၊ မ ၈ ဦး၊ ေသဆုံးသူ က်ား ၁၀ ဦးနဲ႔ ပ်က္စီးဆုံးရႈံးမႈ တန္ဖိုး က်ပ္ ၆၃ သိန္းခန္႔ရွိခဲ့တယ္လို႔ ယာဥ္ထိန္း ရဲတပ္ဖဲြ႔က သိရပါတယ္။သတင္း- ရဲမင္းေခါင္'
 s2 = u'8月28日，网络爆料称，华住集团旗下连锁酒店用户数据疑似发生泄露。从卖家发布的内容看，数据包含华住旗下汉庭、禧玥、桔子、宜必思等10余个品牌酒店的住客信息。泄露的信息包括华住官网注册资料、酒店入住登记的身份信息及酒店开房记录，住客姓名、手机号、邮箱、身份证号、登录账号密码等。卖家对这个约5亿条数据打包出售。第三方安全平台威胁猎人对信息出售者提供的三万条数据进行验证，认为数据真实性非常高。当天下午，华住集 团发声明称，已在内部迅速开展核查，并第一时间报警。当晚，上海警方消息称，接到华住集团报案，警方已经介入调查。'
+
 
 class Evaluate(Callback):
     def __init__(self):
         self.lowest = 1e10
+
     def on_epoch_end(self, epoch, logs=None):
         # 训练过程中观察一两个例子，显示标题质量提高的过程
         print(gen_sent(s1))
